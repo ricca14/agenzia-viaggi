2452367fce795541515dd9eaad467cca89a5581a
Ricaldone Gianluca <g.ricaldone14@gmail.com>
2019-02-09 12:03:43 +0100

    js select nazioni

--- public/js/crea-vacanza.js
+++ public/js/crea-vacanza.js
@@ -4,69 +4,82 @@
 //     $(this).toggleClass('hover_effect');
 // });
 
-$('.continenti .grid_element_box_child').on('click', function () {
-    if ($(this).hasClass('selected')) {
-        console.log('Continente già selezionato');
+function setNazione(id) {
+    div_nazione = document.getElementById('nazione-' + id);
+    if ($(div_nazione).hasClass('selected')) {
+        console.log('Nazione già selezionato');
     }
     else {
-        $('.continenti .grid_element_box_child').removeClass('selected');
-        $(this).addClass('selected');
-        
-        // Gestione etichette piccine
-        var c = $('.continenti .grid_element_box_child_label');
-        for (var i = 0; i < c.length; i++) {
-            if ($(c[i]).hasClass('holiday-green')) {
-                $(c[i]).removeClass('holiday-green');
-                $(c[i]).addClass('holiday-blue');
-            }
-        }
-        var child = $(this).children()[0];
-        $(child).removeClass('holiday-blue');
-        $(child).addClass('holiday-green');
-
-        $('.grid_element_box.continenti').removeClass('holiday-green').removeClass('hover_effect');
-        $($(this).parent()[0]).addClass('hover_effect');
-
-        // CAPIRE COME RENDERE LA COSA PIÙ SELEZIONABILE
-
-        var continente = this.getElementsByTagName('INPUT')[0].value;
-        $('.loader-container').removeClass('d-none');
-        $.ajax({
-            type: "GET",
-            url: '/continente/' + continente,
-            success: function (data) {
-                // Rimuovo tutti gli elementi HTML per poi sostituirli
-                var element = document.getElementById("collapseNazioni");
-                while (element.firstChild) {
-                    element.removeChild(element.firstChild);
-                }
-                $('#collapseNazioni').append(data);
-
-                // Rimuovo loader e rendo visibili nel caso le nazioni
-                $('.title-header-box.nazioni').removeClass('d-none');
-                $('.loader-container').addClass('d-none');
-                if (!$('#collapseNazioni').hasClass('show')) {
-                    $('#collapseNazioni').addClass('show');
-                }
-
-                // Animazione scroll verso nazioni
-                $('html, body').animate({
-                    'scrollTop': $('#anchorNazioni').offset().top
-                }, 1250);
-            },
-            error: function (request, status, error) {
-                $('.loader-container').addClass('d-none');
-                alert(request.responseText);
-            }
-            
-        });
+        $('.nazioni .grid_element_box_child').removeClass('selected');
+        $(div_nazione).addClass('selected');
+        var c = $('.nazioni .grid_element_box_child_label');
+        setSelectedAndLabel(c, $(div_nazione));
     }
-});
+}
 
+function setSelectedAndLabel(list, element) {
+    for (var i = 0; i < list.length; i++) {
+        if ($(list[i]).hasClass('holiday-green')) {
+            $(list[i]).removeClass('holiday-green');
+            $(list[i]).addClass('holiday-blue');
+        }
+    }
+    var child = element.children()[0];
+    $(child).removeClass('holiday-blue');
+    $(child).addClass('holiday-green');
+}
 
-$('.grid_element_box.nazioni').on('click', function () {
+$(document).ready(function () {
 
-    console.log('NAZIONI');
+    $('.continenti .grid_element_box_child').on('click', function () {
+        if ($(this).hasClass('selected')) {
+            console.log('Continente già selezionato');
+        }
+        else {
+            $('.continenti .grid_element_box_child').removeClass('selected');
+            $(this).addClass('selected');
+            
+            // Gestione etichette piccine
+            var c = $('.continenti .grid_element_box_child_label');
+            setSelectedAndLabel(c, $(this));
+
+            $('.grid_element_box.continenti').removeClass('holiday-green').removeClass('hover_effect');
+            $($(this).parent()[0]).addClass('hover_effect');
+
+            
+            var continente = this.getElementsByTagName('INPUT')[0].value;
+            $('.loader-container').removeClass('d-none');
+            $.ajax({
+                type: "GET",
+                url: '/continente/' + continente,
+                success: function (data) {
+                    // Rimuovo tutti gli elementi HTML per poi sostituirli
+                    var element = document.getElementById("collapseNazioni");
+                    while (element.firstChild) {
+                        element.removeChild(element.firstChild);
+                    }
+                    $('#collapseNazioni').append(data);
+
+                    // Rimuovo loader e rendo visibili nel caso le nazioni
+                    $('#anchorNazioni').removeClass('d-none');
+                    $('.loader-container').addClass('d-none');
+                    if (!$('#collapseNazioni').hasClass('show')) {
+                        $('#collapseNazioni').addClass('show');
+                    }
+
+                    // Animazione scroll verso nazioni
+                    $('html, body').animate({
+                        'scrollTop': $('#anchorNazioni').offset().top
+                    }, 1250);
+                },
+                error: function (request, status, error) {
+                    $('.loader-container').addClass('d-none');
+                    alert(request.responseText);
+                } 
+            });
+        }
+    });
+
 
     // if ($(this).hasClass('selected')) {
     //     console.log('Continente già selezionato');
@@ -90,4 +103,6 @@ $('.grid_element_box.nazioni').on('click', function () {
     //     $('.grid_element_box.nazioni').removeClass('holiday-green').removeClass('hover_effect');
     //     $($(this).parent()[0]).addClass('hover_effect');
     // }
+// });
+
 });
