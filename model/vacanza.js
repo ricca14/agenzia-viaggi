4f6ebd3632a1d9f6915bac96059e0fcf15f502b6
Ricaldone Gianluca <g.ricaldone14@gmail.com>
2019-03-01 00:06:12 +0100

    new get vacanze

--- model/vacanza.js
+++ model/vacanza.js
@@ -3,7 +3,7 @@ var log4js = require('log4js');
 var logger = log4js.getLogger();
 logger.level = 'debug';
 const insertVacanzaQuery = "INSERT INTO `richiesta_vacanza` ({campi}, data_richiesta) VALUES ({valori}, now())";
-const defaultQuery = "SELECT v.*, n.`code`, n.`nome`, n.`icon`, c.`nome` AS categoria, c.`url` AS categoria_url, c.`icon` AS categoria_icon, n.`nome` AS nazione_nome, n.`icon` AS nazione_icon, co.`nome` AS continente_nome, c.`icon` AS continente_icon FROM vacanze v left join nazioni n on n.`id` = v.`nazione` left join continenti co on co.`id` = v.`continente` left join categorie c on c.id = v.categoria WHERE v.visibile = 1 and c.visibile = 1 {where} ORDER BY v.`ordine` DESC {limit};";
+const defaultQuery = "SELECT v.*, n.`code`, n.`nome`, n.`icon`, c.`nome` AS categoria, c.`url` AS categoria_url, c.`icon` AS categoria_icon, c.`icon` AS contegoria_icon, n.`nome` AS nazione_nome, n.`icon` AS nazione_icon, t1.`nome` as t1_nome, t2.`nome` as t2_nome, t3.`nome` as t3_nome FROM vacanze v LEFT JOIN nazioni n ON n.`id` = v.`nazione` LEFT JOIN categorie c ON c.id = v.categoria LEFT JOIN alloggi a ON a.id = v.alloggio LEFT JOIN localita l ON l.id = v.localita LEFT JOIN tag t1 ON t1.id = v.tag_1 LEFT JOIN tag t2 ON t2.id = v.tag_2 LEFT JOIN tag t3 ON t3.id = v.tag_3 WHERE v.visibile = 1 AND c.visibile = 1 {where} ORDER BY v.`ordine` DESC {limit};";
 function setDefaultWhere(condition, limit=false) {
     return defaultQuery.replace("{where}", condition).replace("{limit}", limit ? ' LIMIT ' + limit : '');
 } 
